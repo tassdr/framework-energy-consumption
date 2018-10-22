0e1f7b67c80d8346e29932a9c3ea77a7d7b0f7f9
mkanakis <marioskanakis@gmail.com>
2018-10-22 20:22:51 +0200

    Updated data exploration.

--- src/RScripts/DataExploration.R
+++ src/RScripts/DataExploration.R
@@ -1,12 +1,44 @@
-df = read.csv("aggregated_results.csv")
+# author: Marios
 
-colnames(df)
+
+# First we change the directory to the aggregated_results,
+# where all the csv files from each experiment lie inside. 
+getwd()
+setwd("Aggregated_Results")
+
+# We load up all the different datasets, from each experiment, in a list.
+experimentFilenames = list.files(pattern="*.csv")
+data = lapply(experimentFilenames, read.csv)
+
+# We reset the working directory back to normal.
+setwd("..")
+getwd()
+
+
+# Here, for each web front-end framework dataset we acquire only the columns that are important 
+# for our statistical analysis and after we change the column names for convinience.
+# Additionally, we convert "battery" from uWatt to Watt using lapply.
+# Moreover, we create a column "Energy consumption" 
+# which is the product of "duration(seconds)" and "battery(Watts)"
+# Finally we round all numeric elements down to 3-digits
 
 cols = c("subject", "CPU.Load....", "Memory.Usage..KB.", "duration.s.", "Battery.Power...uW.")
-angularjs = df[cols]
-
-
 newnames = c("subject", "cpu", "mem", "duration", "battery")
-colnames(angularjs) = newnames
+
+for (i in 1:length(data))
+{
+  data[[i]] = data[[i]][cols]
+  colnames(data[[i]]) = newnames
+  data[[i]]["battery"] = lapply(data[[i]]["battery"], function(row_val) row_val / 1e+6)
+  data[[i]]["energy_consumption"] = data[[i]]["duration"] * data[[i]]["battery"]
+}
 
 
+# Merge all our experiments into one data frame, 
+# round the digits to 3 
+# and save it as a csv file
+
+finalDataframe = do.call(what = rbind, args = data)
+numeric_columns = sapply(finalDataframe, class) == 'numeric'
+finalDataframe[numeric_columns] = round (finalDataframe[numeric_columns], digits = 3)
+write.csv(finalDataframe, file="aggregated_data.csv")
